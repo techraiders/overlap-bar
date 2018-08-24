d29159c9a1849f9a8728274945e604a68561a5b7
techraiders <hello_navneet@hotmail.com>
2018-08-24 12:41:12 +0530

    Sets common and individual colors for each bars of chart.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Chart from 'chart.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  BarChart : any;
+
+  ngOnInit () {
+    this.BarChart = new Chart('barChart', {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow'],
+        datasets: [
+          {
+            label: 'Number of Votes',
+            data: [9,7,3],
+            backgroundColor: 'rgba(255, 99, 132)',
+            borderColor: 'green',
+            /* Separate colors for each bars
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)'
+            ], */
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        title: {
+          text: 'Bar Chart',
+          display: true
+        },
+        scales: {
+          xAxes: [{
+            stacked: true
+          }],
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            },
+            stacked: true
+          }]
+        }
+      }
+    });
+  }
 }
